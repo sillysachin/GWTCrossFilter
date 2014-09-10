da9fc93dd6e15c5b77c8df80163761f05eb2eef7
Sachin Shekhar R <sachin.r.r@gmail.com>
2014-09-10 17:12:36 +0530

    Exploring Metric Trend Data for Crossfilter.

--- src/com/insanity/sample/client/CrossFilterSample.java
+++ src/com/insanity/sample/client/CrossFilterSample.java
@@ -14,6 +14,7 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.ValueListBox;
 import com.insanity.sample.client.data.flight.FlightDelayData;
+import com.insanity.sample.client.data.metric.MetricData;
 import com.insanity.sample.client.flightservice.JSONFlightService;
 import com.insanity.sample.client.flightservice.JSONFlightServiceAsync;
 import com.square.crossfilter.client.Crossfilter;
@@ -29,148 +30,170 @@ import com.square.crossfilter.client.SingleGroup;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class CrossFilterSample implements EntryPoint {
-	private final JSONFlightServiceAsync jsonFlightService = GWT
-			.create(JSONFlightService.class);
+public class CrossFilterSample implements EntryPoint
+{
+	private final JSONFlightServiceAsync jsonFlightService = GWT.create( JSONFlightService.class );
 
-	public void onModuleLoad() {
+	public void onModuleLoad()
+	{
 		final double start = Duration.currentTimeMillis();
-		jsonFlightService
-				.getFlighDelayData(new AsyncCallback<ArrayList<FlightDelayData>>() {
-					public void onSuccess(ArrayList<FlightDelayData> data) {
-						double end = Duration.currentTimeMillis();
-						GWT.log("Done RPC in " + (end - start) / 1000
-								+ " Seconds");
-						Crossfilter<FlightDelayData> flights = initCrossFilter(data);
-						ArrayList<Dimension<FlightDelayData, ? extends Object>> flightDimensions = initDimensions(flights);
-						initWidgets(flightDimensions);
-					}
+		jsonFlightService.getFlighDelayData( new AsyncCallback<ArrayList<FlightDelayData>>()
+		{
+			public void onSuccess( ArrayList<FlightDelayData> data )
+			{
+				double end = Duration.currentTimeMillis();
+				GWT.log( "Done RPC in " + ( end - start ) / 1000 + " Seconds" );
+				Crossfilter<FlightDelayData> flights = initCrossFilter( data );
+				ArrayList<Dimension<FlightDelayData, ? extends Object>> flightDimensions = initDimensions( flights );
+				initWidgets( flightDimensions );
+			}
 
-					@Override
-					public void onFailure(Throwable caught) {
-						GWT.log("Oops....", caught);
-					}
-				});
+			@Override
+			public void onFailure( Throwable caught )
+			{
+				GWT.log( "Oops....", caught );
+			}
+		} );
+		jsonFlightService.getMetricData( new AsyncCallback<ArrayList<MetricData>>()
+		{
+			public void onSuccess( ArrayList<MetricData> data )
+			{
+				double end = Duration.currentTimeMillis();
+				GWT.log( "Done RPC in " + ( end - start ) / 1000 + " Seconds" );
+				Crossfilter<MetricData> metrics = initMetricCrossFilter( data );
+			}
+
+			@Override
+			public void onFailure( Throwable caught )
+			{
+				GWT.log( "Oops....", caught );
+			}
+		} );
 	}
 
-	protected void initWidgets(
-			ArrayList<Dimension<FlightDelayData, ? extends Object>> flightDimensions) {
+	protected void initWidgets( ArrayList<Dimension<FlightDelayData, ? extends Object>> flightDimensions )
+	{
 	}
 
-	private Crossfilter<FlightDelayData> initCrossFilter(
-			ArrayList<FlightDelayData> data) {
+	private Crossfilter<MetricData> initMetricCrossFilter( ArrayList<MetricData> data )
+	{
+		final Crossfilter<MetricData> metrics = Crossfilter.create( data );
+		return metrics;
+	}
+
+	private Crossfilter<FlightDelayData> initCrossFilter( ArrayList<FlightDelayData> data )
+	{
 		final double startCrossfilter = Duration.currentTimeMillis();
-		final Crossfilter<FlightDelayData> flights = Crossfilter.create(data);
+		final Crossfilter<FlightDelayData> flights = Crossfilter.create( data );
 		final double endCrossfilter = Duration.currentTimeMillis();
-		GWT.log("Done Crossfilter in " + (endCrossfilter - startCrossfilter)
-				/ 1000 + " Seconds");
-		GWT.log("Crossfilter Size " + flights.size());
+		GWT.log( "Done Crossfilter in " + ( endCrossfilter - startCrossfilter ) / 1000 + " Seconds" );
+		GWT.log( "Crossfilter Size " + flights.size() );
 		final double startAddCrossfilter = Duration.currentTimeMillis();
 		FlightDelayData flightDelayData = new FlightDelayData();
-		flightDelayData.setDate(new Date(
-				data.get(0).getDate().getTime() + 36000));
-		flightDelayData.setDelay(data.get(0).getDelay() + 1);
-		flightDelayData.setDestination(data.get(0).getDestination());
-		flightDelayData.setDistance(data.get(0).getDistance());
-		flightDelayData.setOrigin(data.get(0).getOrigin());
-		flights.add(flightDelayData);
+		flightDelayData.setDate( new Date( data.get( 0 ).getDate().getTime() + 36000 ) );
+		flightDelayData.setDelay( data.get( 0 ).getDelay() + 1 );
+		flightDelayData.setDestination( data.get( 0 ).getDestination() );
+		flightDelayData.setDistance( data.get( 0 ).getDistance() );
+		flightDelayData.setOrigin( data.get( 0 ).getOrigin() );
+		flights.add( flightDelayData );
 		final double endAddCrossfilter = Duration.currentTimeMillis();
-		GWT.log("Done AddCrossfilter in "
-				+ (endAddCrossfilter - startAddCrossfilter) / 1000 + " Seconds");
-		GWT.log("Crossfilter Size " + flights.size());
+		GWT.log( "Done AddCrossfilter in " + ( endAddCrossfilter - startAddCrossfilter ) / 1000 + " Seconds" );
+		GWT.log( "Crossfilter Size " + flights.size() );
 		return flights;
 	}
 
-	protected ArrayList<Dimension<FlightDelayData, ? extends Object>> initDimensions(
-			Crossfilter<FlightDelayData> flights) {
+	protected ArrayList<Dimension<FlightDelayData, ? extends Object>> initDimensions( Crossfilter<FlightDelayData> flights )
+	{
 		final double startAddDimensions = Duration.currentTimeMillis();
 
-		final Dimension<FlightDelayData, Date> date = flights
-				.dimension(new DateReducer<FlightDelayData>() {
-					public Date getValue(FlightDelayData object) {
-						return object.getDate();
-					}
-				});
-		final Dimension<FlightDelayData, Double> hour = flights
-				.dimension(new DoubleReducer<FlightDelayData>() {
-					public double getValue(FlightDelayData object) {
-						return object.getDate().getHours()
-								+ object.getDate().getMinutes() / 60;
-					}
-				});
-		Dimension<FlightDelayData, Integer> delay = flights
-				.dimension(new IntReducer<FlightDelayData>() {
-					public int getValue(FlightDelayData object) {
-						return Math.max(-60, Math.min(149, object.getDelay()));
-					}
-				});
-		Dimension<FlightDelayData, Integer> distance = flights
-				.dimension(new IntReducer<FlightDelayData>() {
-					public int getValue(FlightDelayData object) {
-						return Math.min(1999, object.getDistance());
-					}
-				});
-		final Dimension<FlightDelayData, String> origin = flights
-				.dimension(new StringReducer<FlightDelayData>() {
-					public String getValue(FlightDelayData object) {
-						return object.getOrigin();
-					}
-				});
-		final Dimension<FlightDelayData, String> destination = flights
-				.dimension(new StringReducer<FlightDelayData>() {
-					public String getValue(FlightDelayData object) {
-						return object.getDestination();
-					}
-				});
+		final Dimension<FlightDelayData, Date> date = flights.dimension( new DateReducer<FlightDelayData>()
+		{
+			public Date getValue( FlightDelayData object )
+			{
+				return object.getDate();
+			}
+		} );
+		final Dimension<FlightDelayData, Double> hour = flights.dimension( new DoubleReducer<FlightDelayData>()
+		{
+			public double getValue( FlightDelayData object )
+			{
+				return object.getDate().getHours() + object.getDate().getMinutes() / 60;
+			}
+		} );
+		Dimension<FlightDelayData, Integer> delay = flights.dimension( new IntReducer<FlightDelayData>()
+		{
+			public int getValue( FlightDelayData object )
+			{
+				return Math.max( -60, Math.min( 149, object.getDelay() ) );
+			}
+		} );
+		Dimension<FlightDelayData, Integer> distance = flights.dimension( new IntReducer<FlightDelayData>()
+		{
+			public int getValue( FlightDelayData object )
+			{
+				return Math.min( 1999, object.getDistance() );
+			}
+		} );
+		final Dimension<FlightDelayData, String> origin = flights.dimension( new StringReducer<FlightDelayData>()
+		{
+			public String getValue( FlightDelayData object )
+			{
+				return object.getOrigin();
+			}
+		} );
+		final Dimension<FlightDelayData, String> destination = flights.dimension( new StringReducer<FlightDelayData>()
+		{
+			public String getValue( FlightDelayData object )
+			{
+				return object.getDestination();
+			}
+		} );
 		final double endAddDimensions = Duration.currentTimeMillis();
-		GWT.log("Done AddDimensions in "
-				+ (endAddDimensions - startAddDimensions) / 1000 + " Seconds");
+		GWT.log( "Done AddDimensions in " + ( endAddDimensions - startAddDimensions ) / 1000 + " Seconds" );
 
 		ArrayList<Dimension<FlightDelayData, ? extends Object>> dimensions = new ArrayList<>();
-		dimensions.add(delay);
-		dimensions.add(date);
-		dimensions.add(destination);
-		dimensions.add(distance);
-		dimensions.add(origin);
+		dimensions.add( delay );
+		dimensions.add( date );
+		dimensions.add( destination );
+		dimensions.add( distance );
+		dimensions.add( origin );
 
 		final double startAddGroups = Duration.currentTimeMillis();
 
 		SingleGroup<FlightDelayData, Object> all = flights.groupAll();
-		Group<FlightDelayData, Date> dates = date.group(CrossfilterUtil
-				.createGroupingDays());
-		Group<FlightDelayData, Double> hours = hour.group(CrossfilterUtil
-				.createGroupingToFloor());
-		Group<FlightDelayData, Integer> delays = delay.group(CrossfilterUtil
-				.createGroupingToFloor(10));
-		Group<FlightDelayData, Integer> distances = distance
-				.group(CrossfilterUtil.createGroupingToFloor(50));
+		Group<FlightDelayData, Date> dates = date.group( CrossfilterUtil.createGroupingDays() );
+		Group<FlightDelayData, Double> hours = hour.group( CrossfilterUtil.createGroupingToFloor() );
+		Group<FlightDelayData, Integer> delays = delay.group( CrossfilterUtil.createGroupingToFloor( 10 ) );
+		Group<FlightDelayData, Integer> distances = distance.group( CrossfilterUtil.createGroupingToFloor( 50 ) );
 
 		final double endAddGroups = Duration.currentTimeMillis();
-		GWT.log("Done AddGroups in " + (startAddGroups - endAddGroups) / 1000
-				+ " Seconds");
+		GWT.log( "Done AddGroups in " + ( startAddGroups - endAddGroups ) / 1000 + " Seconds" );
 
-		ValueListBox<FlightDelayData> originwidget = new ValueListBox<FlightDelayData>(
-				new Renderer<FlightDelayData>() {
-					public String render(FlightDelayData object) {
-						if (object != null) {
-							return object.getOrigin();
-						} else
-							return "";
-					}
+		ValueListBox<FlightDelayData> originwidget = new ValueListBox<FlightDelayData>( new Renderer<FlightDelayData>()
+		{
+			public String render( FlightDelayData object )
+			{
+				if ( object != null )
+				{
+					return object.getOrigin();
+				}
+				else
+					return "";
+			}
 
-					@Override
-					public void render(FlightDelayData object,
-							Appendable appendable) throws IOException {
-						appendable.append(render(object));
-					};
-				});
-		Collection<FlightDelayData> top5origin = origin.top(5);
-		originwidget.setAcceptableValues(top5origin);
+			@Override
+			public void render( FlightDelayData object, Appendable appendable ) throws IOException
+			{
+				appendable.append( render( object ) );
+			};
+		} );
+		Collection<FlightDelayData> top5origin = origin.top( 5 );
+		originwidget.setAcceptableValues( top5origin );
 		HorizontalPanel panel = new HorizontalPanel();
-		panel.setWidth("75%");
-		panel.add(originwidget);
+		panel.setWidth( "75%" );
+		panel.add( originwidget );
 
-		RootLayoutPanel.get().add(new Bootstrap3DashboardUiBinder());
+		RootLayoutPanel.get().add( new Bootstrap3DashboardUiBinder() );
 		return dimensions;
 	}
 
